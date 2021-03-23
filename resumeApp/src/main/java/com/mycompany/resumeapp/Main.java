9137341d487c4c8d7abdadc5704d5eadaead7f59
User <User@DESKTOP-8IVKM50>
2021-03-23 13:09:20 +0400

    removing unused dependency

--- resumeApp/src/main/java/com/mycompany/resumeapp/Main.java
+++ resumeApp/src/main/java/com/mycompany/resumeapp/Main.java
@@ -1,17 +1,17 @@
 package com.mycompany.resumeapp;
 
+import com.mycompany.bean.User;
 import com.mycompany.resumeapp.dao.inter.EmploymentHistoryDaoInter;
 import com.mycompany.resumeapp.dao.inter.SkillDaoInter;
 import com.mycompany.resumeapp.dao.inter.UserSkillDaoInter;
-import lombok.SneakyThrows;
 /**
  *
  * @author azizg
  * CRUD example
  */
 public class Main {
-    @SneakyThrows
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) throws Exception {
         //loosly coupling 
         UserSkillDaoInter obj = Context.instanceUserSkillDao();
         //System.out.println(obj.getById(1));
